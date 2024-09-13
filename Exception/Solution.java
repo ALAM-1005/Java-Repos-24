6748bfea4930e1741b8c3cb4865aec174c504b21
Md JUNED ALAM <93564319+ALAM-1005@users.noreply.github.com>
2024-09-13 17:39:50 +0000

    initial

--- Exception/Solution.java
+++ Exception/Solution.java
@@ -1,3 +1,15 @@
 public class Solution {
+
+    public static void main(String[] args) {
+        try{
+
+            System.out.println(10/0);
+
+        }
+        catch(){
+
+
+        }
+    }
     
 }
