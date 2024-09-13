8f4a8ddc40c2806438f074fecae272ae7458e5b1
Md JUNED ALAM <93564319+ALAM-1005@users.noreply.github.com>
2024-09-13 17:55:03 +0000

    initial

--- Java_Basics/src/Java_Basics/Mango.java
+++ Java_Basics/src/Java_Basics/Mango.java
@@ -3,15 +3,13 @@ package Java_Basics;
 public class Mango {
 
 {
-
-    
     System.out.println("Mango");
 }
     public static void main(String[] args) {
         System.out.println("main");
 
         Mango m1 = new Mango();
-        Mango m2 = new Mango();
+        Mango m = new Mango();
         Mango m3 = new Mango();
         
         System.out.println(m1);
