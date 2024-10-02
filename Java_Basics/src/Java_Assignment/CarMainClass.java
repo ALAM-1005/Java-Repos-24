311f21229368c3247f51f39875d4713872d80448
Md JUNED ALAM <93564319+ALAM-1005@users.noreply.github.com>
2024-10-02 18:41:27 +0000

    initial

---
+++ Java_Basics/src/Java_Assignment/CarMainClass.java
@@ -0,0 +1,26 @@
+package Java_Assignment;
+
+import java.util.TreeSet;
+
+public class CarMainClass {
+	
+	public static void main(String[] args) {
+
+		
+		Car c1 = new Car(251, "Mahendra");
+		Car c2 = new Car(251, "BMW");
+		Car c3 = new Car(252, "Tayota");
+		
+		TreeSet<Car> t = new TreeSet<Car>();
+		t.add(c1);
+		t.add(c2);
+		t.add(c3);
+		
+		for (Car c : t) {
+			
+			System.out.println(c);
+			
+		}
+		
+	}
+}
