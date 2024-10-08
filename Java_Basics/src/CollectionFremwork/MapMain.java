6dc1c85c1eda6a98b3a9e5a0f4bf2d74d4b645a3
Md JUNED ALAM <93564319+ALAM-1005@users.noreply.github.com>
2024-10-08 01:50:32 +0000

    initial

--- Java_Basics/src/CollectionFremwork/MapMain.java
+++ Java_Basics/src/CollectionFremwork/MapMain.java
@@ -1,9 +1,16 @@
 package CollectionFremwork;
 
+import java.util.LinkedHashMap;
+
+import primitivecasting.narrowing;
+
 public class MapMain {
     
 
     public static void main(String[] args) {
-        
+    Map m =new Map(20);
+    Map m1= new Map(   21);
+
+    LinkedHashMap<Map,String> lks = new LinkedHashMap<Map,String>();
     }
 }
